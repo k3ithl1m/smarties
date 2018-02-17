8dc86dc91febfd8ce87a0707a42f204275cea0c9
david N <dn1216@messiah.edu>
2018-02-17 01:48:59 -0500

    david

--- src/App.js
+++ src/App.js
@@ -12,12 +12,9 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 import WelcomeScreen from "./screens/WelcomeScreen";
-<<<<<<< HEAD
-=======
 import LoginScreen from "./screens/AuthNRegistrationScreens/_LoginScreen";
 import SUGeneralInformationScreen from "./screens/AuthNRegistrationScreens/_SUGeneralInformationScreen";
 // import _NewActivity from "./screens/NewActivity";
->>>>>>> 906ec4cbbf135029d1ee98362ce6d282cfb8b997
 import _MainScreen from "./screens/MainScreens/_MainScreen";
 import _TimeScreen from "./screens/CreateActivityScreens/_TimeScreen";
 
@@ -25,18 +22,11 @@ export default class App extends Component {
   render() {
     const MainNavigator = StackNavigator(
       {
-<<<<<<< HEAD
-        //first: { screen: WelcomeScreen },
-        time: { screen: _TimeScreen},
-        main: { screen: _MainScreen },
-
-=======
         first: { screen: WelcomeScreen },
         // first: { screen: _NewActivity }
         login: { screen: LoginScreen },
         signUp: { screen: SUGeneralInformationScreen },
         main: { screen: _MainScreen }
->>>>>>> 906ec4cbbf135029d1ee98362ce6d282cfb8b997
       },
       {
         headerMode: "none",
@@ -47,19 +37,11 @@ export default class App extends Component {
     );
     return (
       <Provider store={store}>
-<<<<<<< HEAD
-        <View style = {styles.container}>
-          <MainNavigator 
-            style = {{
-              ...Platform.select({
-                android: { width: Dimensions.get("window").width}
-=======
         <View style={styles.container}>
           <MainNavigator
             style={{
               ...Platform.select({
                 android: { width: Dimensions.get("window").width }
->>>>>>> 906ec4cbbf135029d1ee98362ce6d282cfb8b997
               })
             }}
           />
